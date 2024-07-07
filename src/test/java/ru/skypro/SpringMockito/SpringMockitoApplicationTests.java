70a3c1d3e75cbd3afe812f0e7413bf2d9701a3ab
HomeMedia <taknado13@yandex.ru>
2024-07-07 18:54:44 +0700

    start

---
+++ src/test/java/ru/skypro/SpringMockito/SpringMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.SpringMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
